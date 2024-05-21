5e8c59718feda01fdbda1781d454e22de0cf12a9
Zulza010303 <ladderazulza@gmail.com>
2024-05-21 12:38:04 +0700

    first commit

---
+++ Submission A/Problem_A2.py
@@ -0,0 +1,98 @@
+# =====================================================================================
+# PROBLEM A2
+#
+# Build a Neural Network Model for Horse or Human Dataset.
+# The test will expect it to classify binary classes.
+# Your input layer should accept 150x150 with 3 bytes color as the input shape.
+# Don't use lambda layers in your model.
+#
+# The dataset used in this problem is created by Laurence Moroney (laurencemoroney.com).
+#
+# Desired accuracy and validation_accuracy > 83%
+# ======================================================================================
+
+
+import urllib.request
+import zipfile
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.layers import BatchNormalization
+
+def solution_A2():
+    data_url_1 = 'https://github.com/dicodingacademy/assets/releases/download/release-horse-or-human/horse-or-human.zip'
+    urllib.request.urlretrieve(data_url_1, 'horse-or-human.zip')
+    local_file = 'horse-or-human.zip'
+    zip_ref = zipfile.ZipFile(local_file, 'r')
+    zip_ref.extractall('data/horse-or-human')
+
+    data_url_2 = 'https://github.com/dicodingacademy/assets/raw/main/Simulation/machine_learning/validation-horse-or-human.zip'
+    urllib.request.urlretrieve(data_url_2, 'validation-horse-or-human.zip')
+    local_file = 'validation-horse-or-human.zip'
+    zip_ref = zipfile.ZipFile(local_file, 'r')
+    zip_ref.extractall('data/validation-horse-or-human')
+    zip_ref.close()
+
+    TRAINING_DIR = 'data/horse-or-human'
+    train_datagen = ImageDataGenerator(
+        rescale=1.0 / 255.0,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True
+    )
+
+    # YOUR IMAGE SIZE SHOULD BE 150x150
+    train_generator = train_datagen.flow_from_directory(
+        TRAINING_DIR,
+        target_size=(150, 150),
+        batch_size=64,
+        class_mode='binary'
+    )
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+        BatchNormalization(),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        BatchNormalization(),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        BatchNormalization(),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        BatchNormalization(),
+        tf.keras.layers.Dropout(0.5),
+        # YOUR CODE HERE, end with a Neuron Dense, activated by sigmoid
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(optimizer=RMSprop(lr=0.00001), loss='binary_crossentropy', metrics=['accuracy'])
+
+    history = model.fit(
+        train_generator,
+        epochs=20,
+        verbose=1
+    )
+
+    print("Training Accuracy:", history.history['accuracy'][-1])
+
+    validation_datagen = ImageDataGenerator(rescale=1.0 / 255.0)
+
+    validation_generator = validation_datagen.flow_from_directory(
+        'data/validation-horse-or-human',
+        target_size=(150, 150),
+        batch_size=64,
+        class_mode='binary'
+    )
+
+    validation_loss, validation_accuracy = model.evaluate(validation_generator)
+    print("Validation Accuracy:", validation_accuracy)
+
+    return model
+# The code below is to save your model as a .h5 file.
+# It will be saved automatically in your Submission folder.
+if __name__ == '__main__':
+    # DO NOT CHANGE THIS CODE
+    model=solution_A2()
+    model.save("model_A2.h5")
---
+++ Submission C/Problem_C5.py
@@ -0,0 +1,93 @@
+# ============================================================================================
+# PROBLEM C5
+#
+# Build and train a neural network to predict time indexed variables of
+# the multivariate house hold electric power consumption time series dataset.
+# Using a window of past 24 observations of the 7 variables, the model
+# should be trained to predict the next 24 observations of the 7 variables.
+# Use MAE as the metrics of your neural network model.
+# We provided code for normalizing the data. Please do not change the code.
+# Do not use lambda layers in your model.
+#
+# The dataset used in this problem is downloaded from https://archive.ics.uci.edu/dataset/235/individual+household+electric+power+consumption
+#
+# Desired MAE < 0.1 on the normalized dataset.
+# ============================================================================================
+
+import urllib
+import os
+import zipfile
+import pandas as pd
+import tensorflow as tf
+
+# This function downloads and extracts the dataset to the directory that contains this file.
+# DO NOT CHANGE THIS CODE
+# (unless you need to change the URL)
+def download_and_extract_data():
+    url = 'https://raw.githubusercontent.com/dicodingacademy/dicoding_dataset/main/household_power.zip'
+    urllib.request.urlretrieve(url, 'household_power.zip')
+    with zipfile.ZipFile('household_power.zip', 'r') as zip_ref:
+        zip_ref.extractall()
+
+
+# This function normalizes the dataset using min max scaling.
+# DO NOT CHANGE THIS CODE
+def normalize_series(data, min, max):
+    data = data - min
+    data = data / max
+    return data
+
+def windowed_dataset(series, batch_size, n_past=24, n_future=24, shift=1):
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+    dataset = dataset.window(n_past + n_future, shift=shift, drop_remainder=True)
+    dataset = dataset.flat_map(lambda window: window.batch(n_past + n_future))
+    dataset =dataset.shuffle(1000)
+    dataset = dataset.map(lambda window: (window[:-n_future], window[-n_future:, :1]))
+    dataset = dataset.batch(batch_size).prefetch(1)
+    return dataset
+
+def solution_C5():
+    download_and_extract_data()
+    df = pd.read_csv('household_power_consumption.csv', sep=',',
+                     infer_datetime_format=True, index_col='datetime', header=0)
+
+    N_FEATURES = 7
+
+    data = df.values
+    split_time = int(len(data) * 0.5)
+    data = normalize_series(data, data.min(axis=0), data.max(axis=0))
+
+    x_train = data[:split_time]
+    x_valid = data[split_time:]
+
+    BATCH_SIZE = 32
+    N_PAST = 24
+    N_FUTURE = 24
+    SHIFT = 1
+
+    train_set = windowed_dataset(x_train, BATCH_SIZE, N_PAST, N_FUTURE, SHIFT)
+    valid_set = windowed_dataset(x_valid, BATCH_SIZE, N_PAST, N_FUTURE, SHIFT)
+
+    class myCallback(tf.keras.callbacks.Callback):
+        def on_epoch_end(self, epoch, logs=None):
+            if(logs.get('mae')<0.09 and logs.get('val_mae')<0.09):
+                print("\nMAE training dan validasi telah lebih kecil dari 0.9")
+                self.model.stop_training = True
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.LSTM(50, activation='relu', input_shape=[N_PAST, N_FEATURES]),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(50, activation='relu'),
+        tf.keras.layers.Dense(N_FUTURE)
+    ])
+
+    model.compile(loss='mae', metrics=['mae'], optimizer='adam')
+    model.fit(train_set, epochs=10, validation_data=valid_set, callbacks=[myCallback()], verbose=1)
+    return model
+
+# The code below is to save your model as a .h5 file.
+# It will be saved automatically in your Submission folder.
+if __name__ == '__main__':
+    # DO NOT CHANGE THIS CODE
+    model = solution_C5()
+    model.save("model_C5.h5")
